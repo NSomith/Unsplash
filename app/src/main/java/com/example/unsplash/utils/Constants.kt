2b53f8b2a88fd22ccad4671b53b7646e01b14f44
N Somith Singh <ningombamsomith@gmail.com>
2021-06-20 23:26:04 +0530

    views

--- app/src/main/java/com/example/unsplash/utils/Constants.kt
+++ app/src/main/java/com/example/unsplash/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.example.unsplash.utils
 
 class Constants {
     companion object{
-        const val API_KEY = "FjJT3eTpiY6UQ2uqSnc-gfAy_xDm4AYVru2cPBj_QWk"
+        const val API_KEY = "Your api key"
         const val BASE_URL = "https://api.unsplash.com"
     }
 }
